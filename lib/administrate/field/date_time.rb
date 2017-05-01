89a1f406f16c553102a973f1d33a01f3b07fce9a
Ben A. Morgan <ben@benmorgan.io>
2017-05-01 15:28:37 -0400

    convert some views to render_page

--- lib/administrate/field/date_time.rb
+++ lib/administrate/field/date_time.rb
@@ -11,10 +11,25 @@ module Administrate
         I18n.localize(data, format: format, default: data)
       end
 
+      def render_page
+        case page
+        when :index then render_index
+        when :show then render_show
+        end
+      end
+
       private
 
       def format
         options.fetch(:format, :default)
+      end
+
+      def render_index
+        date if data
+      end
+
+      def render_show
+        datetime if data
       end
     end
   end
